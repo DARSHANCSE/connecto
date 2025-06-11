3f524a5cf7870d9ce1b58196110be24e3323b792
Darshan-Udchalo <sdarshan.aids2023@citchennai.net>
2025-06-11 22:11:36 +0530

    websocket messaging halfway done

--- backend/src/Routers/backRouters.ts
+++ backend/src/Routers/backRouters.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { createGroupsHandler, getGroupsHandler, loginHandler, registerHandler } from "../Components/handlers";
 const backrouter = Router();
 
-//hello
 backrouter.post("/login", loginHandler);
 backrouter.post("/register", registerHandler);
 backrouter.get("/getgroups/:userId", getGroupsHandler);
